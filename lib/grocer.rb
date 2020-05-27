205724a4d0c8d4bbddad0874f0576190f6b9dba4
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-27 20:57:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,6 +16,9 @@ def consolidate_cart(cart)
     if !new_cart.include?(e)
       e[:count] = 1
       new_cart.push(e)
+    else
+      index = new_cart.index(e)
+      new_cart[index][:count] += 1
   #  else
     #  temp_item = find_item_by_name_in_collection(e[:item], new_cart)
     #  temp_item[:count] += 1
@@ -24,5 +27,5 @@ def consolidate_cart(cart)
     #  new_cart.push(e with e[:count] = 1)
     end
   end
-
+  new_cart
 end
